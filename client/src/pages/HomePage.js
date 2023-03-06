52461e8a8e1d47766a47723ce772cdf2a552e5cf
julescwrt <julianeschawert@gmail.com>
2023-03-06 22:27:27 +0100

    fixed landing page

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -9,13 +9,16 @@ const HomePage = () => {
   };
 
   return (
-    <div>
-      <div className="landingtext">
+    <div className="d-flex flex-column align-items-center justify-content-center vh-100">
+      <div className="landingtext text-center">
         Create and share <br></br>design ideas with <br></br>your friends.
         <br></br>
-        <button className="signup">Sign up</button>
+        <button className="signup mb-5" onClick={handleSignUp}>
+          Sign up
+        </button>
+        <span></span>
       </div>
-      <div className="container">
+      <div className="container d-flex justify-content-center mx-5">
         <img src={Img1} alt="image1" id="img1" />
         <img src={Img2} alt="image2" id="img2" />
       </div>

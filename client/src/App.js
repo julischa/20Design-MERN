2899dab038af7c7b12e3a79e0acbc8479602d542
julescwrt <julianeschawert@gmail.com>
2023-03-31 15:32:16 +0200

    Update App.css

--- client/src/App.js
+++ client/src/App.js
@@ -58,7 +58,7 @@ function App() {
           <Route path="/messages" element={<Messages />} />
         </Routes>
       </BrowserRouter>
-      <Footer />
+      {/* <Footer /> */}
       <Github />
     </AuthContextProvider>
   );
